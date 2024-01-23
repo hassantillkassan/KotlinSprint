f8ce7f6c0dbff5d92c29e8d379a322780694d21b
Андрей <@jay_gatz>
2024-01-24 01:16:08 +0700

    KS-1-3 task solution

---
+++ src/main/kotlin/lesson1/lesson1-task4.kt
@@ -0,0 +1,10 @@
+package org.example.lesson1
+
+fun main () {
+    val length: Long = 40868600000
+    val age: UByte = 27U
+    val partOfDay: Float = 0.075f
+    val seconds: UShort = 6480U
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+}
