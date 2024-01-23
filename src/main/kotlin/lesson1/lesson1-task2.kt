24aff892216d09befeb53b574c1609f8cd44eb50
Андрей <@jay_gatz>
2024-01-24 00:59:46 +0700

    KS-1-2 task solution

---
+++ src/main/kotlin/lesson1/lesson1-task2.kt
@@ -0,0 +1,18 @@
+package org.example.lesson1
+
+fun main() {
+    val numberOfOrders: Byte = 75
+    val appreciationText: String = "Спасибо за покупку!"
+
+    println(numberOfOrders)
+    println(appreciationText)
+
+    var numberOfEmployees: Short = 2000
+
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+
+    println(numberOfEmployees)
+
+}
