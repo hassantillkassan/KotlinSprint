6a1f727d5c9ce779530ba69e5551f631e131cbf7
Андрей <@jay_gatz>
2024-01-31 03:07:27 +0700

    KS-1-5 refactor

--- src/main/kotlin/lesson1/lesson1-task5.kt
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -1,11 +1,13 @@
 package org.example.lesson1
 
 fun main () {
-    val hourConst: UByte = 60U
-    var seconds: UInt = 6480U
-    val minutes: UInt = (seconds / hourConst)
-    val hours = (minutes / hourConst)
+    val BASIS_TIME: Byte = 60
+    var seconds: Int = 6480
+    var minutes: Int = (seconds / BASIS_TIME)
+    val hours = (minutes / BASIS_TIME)
 
-    seconds %= hourConst
-    print("0" + hours + ":" + (minutes - hourConst) + ":" + seconds + "0")
+    seconds %= BASIS_TIME
+    minutes %= BASIS_TIME
+
+    println(String.format("%02d:%02d:%02d", hours, minutes, seconds))
 }
