9f98ef1109a02366e891833dce702eddcb80b67f
Андрей <@jay_gatz>
2024-01-31 21:43:26 +0700

    KS-1-5 refactor add const

--- src/main/kotlin/lesson1/lesson1-task5.kt
+++ src/main/kotlin/lesson1/lesson1-task5.kt
@@ -1,7 +1,8 @@
 package org.example.lesson1
 
+const val BASIS_TIME: Byte = 60
+
 fun main () {
-    val BASIS_TIME: Byte = 60
     var seconds: Int = 6480
     var minutes: Int = (seconds / BASIS_TIME)
     val hours = (minutes / BASIS_TIME)
