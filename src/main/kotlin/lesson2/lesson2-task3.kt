7587dfc11d5b0a01e2106c0bd6e587db6dd97e52
Андрей <@jay_gatz>
2024-02-01 22:54:00 +0700

    KS-2-3 refactor

--- src/main/kotlin/lesson2/lesson2-task3.kt
+++ src/main/kotlin/lesson2/lesson2-task3.kt
@@ -1,20 +1,15 @@
 package lesson2
 
+const val BASIS_TIME = 60
+
 fun main() {
     val hourOfDeparture = 9
     val minuteOfDeparture = 39
     val travelTime = 457
 
-    var hourOfArrival = travelTime / 60 + hourOfDeparture
-    var minuteOfArrival: Int
-
-    if ((travelTime % 60) < (60 - minuteOfDeparture)) {
-        minuteOfArrival = travelTime % 60 + minuteOfDeparture
-    } else {
-        hourOfArrival++
-        minuteOfArrival = (travelTime + minuteOfDeparture) % 60
-    }
-
+    var minuteOfArrival = minuteOfDeparture + travelTime
+    val hourOfArrival = hourOfDeparture + minuteOfArrival / BASIS_TIME
+    minuteOfArrival %= BASIS_TIME
 
     println(String.format("%02d:%02d", hourOfArrival, minuteOfArrival))
 }
