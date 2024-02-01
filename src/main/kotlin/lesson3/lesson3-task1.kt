6b2f3e602b04cfa96be1fc7769800585a570f4a7
Андрей <@jay_gatz>
2024-02-02 02:33:26 +0700

    KS-3-1 task solution

---
+++ src/main/kotlin/lesson3/lesson3-task1.kt
@@ -0,0 +1,12 @@
+package lesson3
+
+fun main() {
+    val userName = "Дмитрий Анатольевич"
+    var greeting = "Добрый день"
+
+    println("$greeting, $userName!")
+
+    greeting = "Добрый вечер"
+
+    println("$greeting, $userName!")
+}
