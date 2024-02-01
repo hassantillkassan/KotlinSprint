936336fadce1567df503ecc52d11103de3d1048e
Андрей <@jay_gatz>
2024-02-02 02:49:04 +0700

    KS-3-2 task solution

---
+++ src/main/kotlin/lesson3/lesson3-task2.kt
@@ -0,0 +1,15 @@
+package lesson3
+
+fun main() {
+    val name = "Татьяна"
+    var surname = "Андреева"
+    val patronymic = "Сергеевна"
+    var age = 20
+
+    println("$surname $name $patronymic, $age")
+
+    surname = "Сидорова"
+    age = 22
+
+    println("$surname $name $patronymic, $age")
+}
