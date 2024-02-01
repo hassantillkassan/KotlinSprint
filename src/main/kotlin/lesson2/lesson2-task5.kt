b2300112d83bc2b25bed4309f60365b9cbead84e
Андрей <@jay_gatz>
2024-02-01 23:32:17 +0700

    KS-2-4 task solution

---
+++ src/main/kotlin/lesson2/lesson2-task5.kt
@@ -0,0 +1,11 @@
+package lesson2
+
+import kotlin.math.pow
+
+fun main() {
+    val investment = 70000
+    val interestRate = 1.167
+    val depositPeriod = 20
+
+    print(String.format("%.3f", investment * (interestRate).pow(depositPeriod)))
+}
