2191003423e62115a300814a8692ff70b1fe7138
Андрей <@jay_gatz>
2024-02-02 23:45:23 +0700

    KS-2-5 refactor

--- src/main/kotlin/lesson2/lesson2-task5.kt
+++ src/main/kotlin/lesson2/lesson2-task5.kt
@@ -4,9 +4,12 @@ import kotlin.math.pow
 
 fun main() {
     val investment = 70000
-    val interestRate = 1.167
     val depositPeriod = 20
+    var interestRate = 16.7
 
-    print(String.format("%.3f", investment * (interestRate).pow(depositPeriod)))
+    interestRate /= 100
+
+
+    print(String.format("%.3f", investment * (++interestRate).pow(depositPeriod)))
 
 }
