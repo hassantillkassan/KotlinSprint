4da45d5e2bab375838d1bbb12c2ac4c3add219f0
Андрей <@jay_gatz>
2024-02-02 23:41:27 +0700

    KS-2-4 refactor

--- src/main/kotlin/lesson2/lesson2-task4.kt
+++ src/main/kotlin/lesson2/lesson2-task4.kt
@@ -3,7 +3,9 @@ package lesson2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    val  buff =  0.2f
+    var  buff: Float =  20f
+
+    buff /= 100
 
     println("Количество бонусной кристаллической руды: " + (crystalOre * buff).toInt())
     println("Количество бонусной железной руды: " + (ironOre * buff).toInt())
