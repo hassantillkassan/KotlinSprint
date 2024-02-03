5128b69b1197880b432831548bcf18dfb9658823
Андрей <@jay_gatz>
2024-02-03 23:38:23 +0700

    KS-2-4 refactor: added coeff

--- src/main/kotlin/lesson2/lesson2-task4.kt
+++ src/main/kotlin/lesson2/lesson2-task4.kt
@@ -3,10 +3,10 @@ package lesson2
 fun main() {
     val crystalOre = 7
     val ironOre = 11
-    var  buff: Float =  20f
+    val  buff =  20f
 
-    buff /= 100
+    val coeff = buff / 100
 
-    println("Количество бонусной кристаллической руды: " + (crystalOre * buff).toInt())
-    println("Количество бонусной железной руды: " + (ironOre * buff).toInt())
+    println("Количество бонусной кристаллической руды: " + (crystalOre * coeff).toInt())
+    println("Количество бонусной железной руды: " + (ironOre * coeff).toInt())
 }
