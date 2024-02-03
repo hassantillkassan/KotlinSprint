90640df785023d04c5413a797b829f20e92242c5
Андрей <@jay_gatz>
2024-02-04 03:53:15 +0700

    KS-4-4 task solution

--- src/main/kotlin/lesson4/lesson4-task4.kt
+++ src/main/kotlin/lesson4/lesson4-task4.kt
@@ -11,8 +11,4 @@ fun main() {
         |Упражнения для пресса: ${String.format("%5s", evenCheck != 0)}
     """.trimMargin()
     println(trainingProgram)
-
-    println(String.format("%3d %8d", 488, 488))
-    println(String.format("%3d %8d", 489, 44499))
-    println(String.format("%3d %8d", 490, 70070))
 }
