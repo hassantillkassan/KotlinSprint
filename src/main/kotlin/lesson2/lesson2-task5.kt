4c77c461050021ee45f510f9cc929b16ef5e871d
Андрей <@jay_gatz>
2024-02-03 23:40:40 +0700

    KS-2-5 refactor: added coeff

--- src/main/kotlin/lesson2/lesson2-task5.kt
+++ src/main/kotlin/lesson2/lesson2-task5.kt
@@ -5,11 +5,11 @@ import kotlin.math.pow
 fun main() {
     val investment = 70000
     val depositPeriod = 20
-    var interestRate = 16.7
+    val interestRate = 16.7
 
-    interestRate /= 100
+    var coeff = interestRate / 100
 
 
-    print(String.format("%.3f", investment * (++interestRate).pow(depositPeriod)))
+    print(String.format("%.3f", investment * (++coeff).pow(depositPeriod)))
 
 }
