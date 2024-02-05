1ab86b6ae7885d52858ba34302c946aa496653c9
Андрей <@jay_gatz>
2024-02-06 01:52:51 +0700

    KS-4-3 refactor: added booleans "FAVORABLE_SEASON_IS_SUMMER"  "isSummer"

--- src/main/kotlin/lesson4/lesson4-task3.kt
+++ src/main/kotlin/lesson4/lesson4-task3.kt
@@ -3,13 +3,13 @@ package lesson4
 const val IS_SUNNY = true
 const val IS_TENT_OPEN = true
 const val AIR_HUMIDITY = 20
-const val SEASON = "Не зима"
+const val FAVORABLE_SEASON_IS_SUMMER = true
 
 fun main() {
     val isSunny = true
     val isTentOpen = true
     val airHumidity = 20
-    val timeOfYear = "Зима"
+    val isSummer = false
 
-    println("Благоприятные ли условия сейчас для роста бобовых? ${(isSunny == IS_SUNNY) and (isTentOpen == IS_TENT_OPEN) and (airHumidity == AIR_HUMIDITY) and (timeOfYear == SEASON)}")
+    println("Благоприятные ли условия сейчас для роста бобовых? ${(isSunny == IS_SUNNY) and (isTentOpen == IS_TENT_OPEN) and (airHumidity == AIR_HUMIDITY) and (isSummer == FAVORABLE_SEASON_IS_SUMMER)}")
 }
