0f1582f9e18cf619b8007510aff0b00e75eefc33
Андрей <@jay_gatz>
2024-02-06 01:43:46 +0700

    KS-4-4 refactor: "String.format" removed

--- src/main/kotlin/lesson4/lesson4-task4.kt
+++ src/main/kotlin/lesson4/lesson4-task4.kt
@@ -1,14 +1,14 @@
 package lesson4
 
 fun main() {
-    val dayOfTraining = 4
+    val dayOfTraining = 5
     val evenCheck = dayOfTraining % 2
 
     val trainingProgram = """
-        |Упражнения для рук: ${String.format("%8s", evenCheck != 0)}
-        |Упражнения для ног: ${String.format("%8s", evenCheck == 0)}
-        |Упражнения для спины: ${String.format("%6s", evenCheck == 0)}
-        |Упражнения для пресса: ${String.format("%5s", evenCheck != 0)}
+        |Упражнения для рук:    ${evenCheck !== 0}
+        |Упражнения для ног:   ${evenCheck == 0}
+        |Упражнения для спины: ${evenCheck == 0}
+        |Упражнения для пресса: ${evenCheck != 0}
     """.trimMargin()
     println(trainingProgram)
 }
