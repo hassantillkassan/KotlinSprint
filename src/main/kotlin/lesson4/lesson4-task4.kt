570d795dd13049d17d5fcdd88e4ae346a0221b05
Андрей <@jay_gatz>
2024-02-06 23:22:42 +0700

    KS-4-4 refactor: "evenCheck" changed to boolean

--- src/main/kotlin/lesson4/lesson4-task4.kt
+++ src/main/kotlin/lesson4/lesson4-task4.kt
@@ -2,13 +2,14 @@ package lesson4
 
 fun main() {
     val dayOfTraining = 5
-    val evenCheck = dayOfTraining % 2
+    val evenCheck = dayOfTraining % 2 == 0
 
     val trainingProgram = """
-        |Упражнения для рук:    ${evenCheck !== 0}
-        |Упражнения для ног:   ${evenCheck == 0}
-        |Упражнения для спины: ${evenCheck == 0}
-        |Упражнения для пресса: ${evenCheck != 0}
-    """.trimMargin()
+        Упражнения для рук:    ${!evenCheck}
+        Упражнения для ног:    $evenCheck
+        Упражнения для спины:  $evenCheck
+        Упражнения для пресса:  ${!evenCheck}
+    """.trimIndent()
+
     println(trainingProgram)
 }
