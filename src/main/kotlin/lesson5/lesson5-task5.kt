25716f7fa9eec7c7ecc612b3e2d1ea5c85d7ae56
Андрей <@jay_gatz>
2024-02-09 00:25:55 +0700

    KS-5-5: refactor. removed method call "when" and operator "!!"

--- src/main/kotlin/lesson5/lesson5-task5.kt
+++ src/main/kotlin/lesson5/lesson5-task5.kt
@@ -7,7 +7,7 @@ fun main() {
     println("Загаданные числа: $guessedNumbers") // Служебный код для проверки
 
     print("Попробуйте угадать три числа от 0 до 42. Введите ваши числа через пробел: ")
-    val (firstTry, secondTry, thirdTry) = readln()!!.split(" ")
+    val (firstTry, secondTry, thirdTry) = readln().split(" ")
 
     val guessingNumbers = listOf(
         firstTry.toInt(),
@@ -18,12 +18,12 @@ fun main() {
     val intersectedList = guessedNumbers.intersect(guessingNumbers)
     val winningNumbers = intersectedList.size
 
-    val gameResult = when (winningNumbers) {
+    when (winningNumbers) {
         3 -> println("Вы угадали все числа и выиграли джекпот!")
         2 -> println("Вы угадали два числа. Так заберите ваш крупный приз!")
         1 -> println("Совпало всего одно число. Утешительный приз уже на вашей карточке")
         else -> println("Вы не угадали ни одного числа. На выход!")
     }
 
-    println("$gameResult\nКстати, выигрышные числа были $guessedNumbers")
+    println("Кстати, выигрышные числа были $guessedNumbers")
 }
