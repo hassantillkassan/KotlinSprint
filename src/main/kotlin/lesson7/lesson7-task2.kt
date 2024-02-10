49c8679454848c271caa7b735a2492173f2f00fd
Андрей <@jay_gatz>
2024-02-11 02:13:02 +0700

    KS-7-2: refactor. all authorisation code wrapped in "do-while" loop

--- src/main/kotlin/lesson7/lesson7-task2.kt
+++ src/main/kotlin/lesson7/lesson7-task2.kt
@@ -3,18 +3,13 @@ package lesson7
 import kotlin.random.Random
 
 fun main() {
-    var smsCode = Random.nextInt(1000, 9999)
-    println("Ваш код авторизации $smsCode")
-
-    print("Введите код, чтобы авторизироваться: ")
-    var enteredCode = readln().toInt()
-
-    while (enteredCode != smsCode) {
-        smsCode = Random.nextInt(1000, 9999)
+    do {
+        val smsCode = Random.nextInt(1000, 9999)
         println("Ваш код авторизации $smsCode")
 
         print("Введите код, чтобы авторизироваться: ")
-        enteredCode = readln().toInt()
-    }
+        val enteredCode = readln().toInt()
+    } while (enteredCode != smsCode)
+
     println("Добро пожаловать!")
 }
