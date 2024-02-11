6672b2f4ec53d50e42430d229cd529525f67f337
Андрей <@jay_gatz>
2024-02-12 02:02:48 +0700

    KS-8-1: refactor. added "sum()" function instead of for-loop

--- src/main/kotlin/lesson8/lesson8-task1.kt
+++ src/main/kotlin/lesson8/lesson8-task1.kt
@@ -4,7 +4,6 @@ import kotlin.random.Random
 
 fun main() {
     var adViews = intArrayOf()
-    var numberOfViews = 0
 
     for (i in 1..7) adViews += Random.nextInt(0, 100)
     println(adViews.contentToString())
