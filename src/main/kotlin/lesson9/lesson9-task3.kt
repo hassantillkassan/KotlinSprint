9539aae7f463054bad9d55a659a3f88289007538
Андрей <@jay_gatz>
2024-02-15 05:19:16 +0700

    KS-9-3: refactor. added "if-else" in forEach-loop

--- src/main/kotlin/lesson9/lesson9-task3.kt
+++ src/main/kotlin/lesson9/lesson9-task3.kt
@@ -1,14 +1,15 @@
 package lesson9
 
 fun main() {
-    val mapOfIngredients = mutableMapOf("Яиц" to 2, "молока" to 50, "масла" to 15)
-    println(mapOfIngredients)
+    val mapOfIngredients = mapOf("Яиц" to 2, "молока" to 50, "масла" to 15)
 
     print("Введите количество порций, которое хотите приготовить: ")
     val enteredNumberOfPortions = readln().toInt()
 
     print("На $enteredNumberOfPortions порций вам понадобится: ")
+
     mapOfIngredients.forEach {
-        print("${it.key} – ${it.value * enteredNumberOfPortions}, ")
+        if (it.key == "масла") print("${it.key} – ${it.value * enteredNumberOfPortions}.")
+        else print("${it.key} – ${it.value * enteredNumberOfPortions}, ")
     }
 }
