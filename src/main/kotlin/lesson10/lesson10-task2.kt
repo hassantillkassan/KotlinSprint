6bead645ffe2d89c624ff344e5c6dd144d50a2c9
Андрей <@jay_gatz>
2024-02-18 04:49:54 +0700

    KS-10-2: refactor. rewritten function 'checkData'

--- src/main/kotlin/lesson10/lesson10-task2.kt
+++ src/main/kotlin/lesson10/lesson10-task2.kt
@@ -4,10 +4,9 @@ fun main() {
     println("Придумайте логин и пароль, содержащие в себе хотя бы 4 символа")
     val enteredName = readln()
     val enteredPassword = readln()
-    checkData(userName = enteredName, userPassword = enteredPassword)
-}
 
-fun checkData(userName: String?, userPassword: String?) {
-    if ((userName!!.length < 4) or (userPassword!!.length < 4))
+    if (!checkData(enteredName) or !checkData(enteredPassword))
         println("Логин или пароль недостаточно длинные")
 }
+
+fun checkData(userData: String?): Boolean = userData!!.length >= 4
