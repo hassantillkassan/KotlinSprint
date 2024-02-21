15ac8c401230885ddec1b061c50f802527210193
Андрей <@jay_gatz>
2024-02-22 04:46:43 +0700

    KS-11-4: task solution

---
+++ src/main/kotlin/lesson_11/lesson11-task4.kt
@@ -0,0 +1,45 @@
+package lesson_11
+
+class RecipeCategory(
+    val categoryName: String,
+    val cover: String,
+    val id: Int,
+    val dishName: String,
+    val dishImage: String,
+) {
+    class Recipe(
+        private val recipeName: String,
+        private var numberOfPortions: Int,
+        val listOfIngredients: List<Ingredient> = listOf(),
+        val cookingMethod: String,
+        private var inFavorites: Boolean = false,
+    ) {
+        fun addToFavorites() {
+            println("Рецепт блюда $recipeName добавлен в \"Избранное\"")
+            inFavorites = true
+        }
+
+        fun removeToFavorites() {
+            println("Рецепт блюда $recipeName удалён из \"Избранного\"")
+            inFavorites = false
+        }
+
+        fun changeNumberOfPortionsBySlider() {
+            numberOfPortions++
+            numberOfPortions--
+        }
+    }
+
+    data class Ingredient(
+        val name: String,
+        val gram: Int,
+    )
+
+    fun moveToRecipeCategories() {
+        println("Вы перешли на страницу с категориями")
+    }
+
+    fun moveToFavorites() {
+        println("Вы перешли на страницу с избранными рецептами")
+    }
+}
