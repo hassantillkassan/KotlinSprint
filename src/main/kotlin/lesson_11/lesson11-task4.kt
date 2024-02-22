db1e0deb6dcde21242dd0b1f0c9f5e818da8ac77
Андрей <@jay_gatz>
2024-02-23 02:06:31 +0700

    KS-11-4: refactor. moved classes outside 'Recipe'-class

--- src/main/kotlin/lesson_11/lesson11-task4.kt
+++ src/main/kotlin/lesson_11/lesson11-task4.kt
@@ -4,37 +4,9 @@ class RecipeCategory(
     val categoryName: String,
     val cover: String,
     val id: Int,
-    val dishName: String,
+    private val dishName: String,
     val dishImage: String,
 ) {
-    class Recipe(
-        private val recipeName: String,
-        private var numberOfPortions: Int,
-        val listOfIngredients: List<Ingredient> = listOf(),
-        val cookingMethod: String,
-        private var inFavorites: Boolean = false,
-    ) {
-        fun addToFavorites() {
-            println("Рецепт блюда $recipeName добавлен в \"Избранное\"")
-            inFavorites = true
-        }
-
-        fun removeToFavorites() {
-            println("Рецепт блюда $recipeName удалён из \"Избранного\"")
-            inFavorites = false
-        }
-
-        fun changeNumberOfPortionsBySlider() {
-            numberOfPortions++
-            numberOfPortions--
-        }
-    }
-
-    data class Ingredient(
-        val name: String,
-        val gram: Int,
-    )
-
     fun moveToRecipeCategories() {
         println("Вы перешли на страницу с категориями")
     }
@@ -42,4 +14,37 @@ class RecipeCategory(
     fun moveToFavorites() {
         println("Вы перешли на страницу с избранными рецептами")
     }
+
+    fun selectRecipe() {
+        println("Открылась страница с рецептом $dishName")
+    }
 }
+
+class Recipe(
+    private val recipeName: String,
+    private var numberOfPortions: Int,
+    val listOfIngredients: List<Ingredient> = listOf(),
+    val cookingMethod: String,
+    private var inFavorites: Boolean = false,
+) {
+    fun addToFavorites() {
+        println("Рецепт блюда $recipeName добавлен в \"Избранное\"")
+        inFavorites = true
+    }
+
+    fun removeToFavorites() {
+        println("Рецепт блюда $recipeName удалён из \"Избранного\"")
+        inFavorites = false
+    }
+
+    fun changeNumberOfPortionsBySlider() {
+        numberOfPortions++
+        numberOfPortions--
+    }
+}
+
+data class Ingredient(
+    val name: String,
+    val gram: Int,
+)
+
